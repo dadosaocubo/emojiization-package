fa50b911294406593d5c31199dc8299ad53fad13
Tiago Dias <diasctiago@gmail.com>
2022-03-15 07:44:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="O pacote emojiization foi desenvolvido pelo time do dadosaocubo.com para tratamento de textos com Emojis",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dadosaocubo/emojiization-package"
+    url="https://github.com/dadosaocubo/emojiization-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',

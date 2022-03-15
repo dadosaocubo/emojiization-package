d7a21da6ae2d57f2af95a7aa1c8dcb7d39c573d7
Tiago Dias <diasctiago@gmail.com>
2022-03-15 07:25:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="emojiization",
     version="0.0.1",
-    author="Tiago Dias",
-    author_email="diasctiago@gmail.com",
-    description="My short description",
+    author="Dados ao Cubo",
+    author_email="tiago@dadosaocubo.com",
+    description="O pacote emojiization foi desenvolvido pelo time do dadosaocubo.com para tratamento de textos com Emojis",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/diasctiago/dio/tree/main/criando_pacotes_python"
+    url="https://github.com/dadosaocubo/emojiization-package"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
